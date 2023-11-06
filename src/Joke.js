f9532e6d36cc41992e39600448114f640d1c1247
Ryan <dowdryan8@gmail.com>
2023-11-06 15:00:02 -0500

    first commit

---
+++ src/Joke.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Joke.css";
+
+
+function Joke({id, vote, votes, text}) {
+  function upVote(evt) { vote(id, +1); }
+  function downVote(evt) { vote(id, -1); }
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+        <button onClick={downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+        {votes}
+      </div>
+      <div className="Joke-text">{text}</div>
+    </div>
+  )
+}
+
+export default Joke;
